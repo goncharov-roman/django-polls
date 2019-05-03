419017e3626c7bb3e00fbe2bf42203a689edc040
goncharov-roman <romg92@mail.ru>
2019-05-03 17:46:58 +0300

    ResultsView fixed && covered with tests

--- polls/tests.py
+++ polls/tests.py
@@ -63,7 +63,7 @@ class QuestionIndexViewTests(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
-            ['<Question: Past question 1.>', '<Question: Past question 2.>']
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
 
 class QuestionDetailViewTests(TestCase):
@@ -75,6 +75,19 @@ class QuestionDetailViewTests(TestCase):
 
     def test_past_question(self):
         past_question = create_question(question_text='Past question.', days=-5)
-        url = reverse('polls:detail', args=(future_question.id,))
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+class QuestionResultsViewTests(TestCase):
+    def test_future_question(self):
+        future_question = create_question(question_text='Future question.', days=5)
+        url = reverse('polls:results', args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        past_question = create_question(question_text='Past question.', days=-5)
+        url = reverse('polls:results', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
